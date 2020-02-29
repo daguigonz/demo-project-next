f9778c72a8f6559de44d761e893f2786b7736326
Aguirre <daniel@combativa.com>
2020-02-28 22:54:48 -0500

    Clase 2, se implementó demode renderizar imagen, se aplicó jsx css y jsx Global

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 export default class extends React.Component{
   render(){
     return <div>
+      
+      <img class="cbv-logo" src="/static/combativa.svg" alt="combativa"/>
+      
       <h1>  Hola Mundo !! </h1>
       <p>bienvenido texto de prueba </p>
+      <style jsx>{`
+        h1 {
+          color: #ff312a;
+        }
+        :global(div p) {
+          color: #fff;
+        }
+        img {
+          max-width: 50%;
+          display: block;
+          margin: 0 auto;
+        }
+        .cbv-logo{
+          max-width: 50%;
+          display: block;
+          margin: 0 auto;
+        }
+
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          background:#022039;
+        }
+      `}</style>
+ 
+
       </div>
   }
 }
